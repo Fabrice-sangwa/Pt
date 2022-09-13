3505e80124b67cefd362064172790c36ac11a6d4
Fabrice-sangwa <19sm476@esisalama.org>
2022-09-13 19:29:38 +0200

    Initialisation du projet

---
+++ src/projett/connection.java
@@ -0,0 +1,25 @@
+package projett;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class connection {
+
+    static PreparedStatement connectbd(String select__from_article_WHERE_Designation_) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Connection conn = null;
+
+    public static Connection connectbd() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/serniehkdb", "root", "");
+            return conn;
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+
+}
